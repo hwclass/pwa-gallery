205ac3566a3d6e08bc0b7a7db18714427b64b0d4
Baris Güler <baris.gueler@blacklane.com>
2018-01-28 12:29:10 +0100

    added header, layout components with service worker fixes #1 #2 #3

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default class extends React.Component {
+  render() {
+    return (
+      <header>Header</header>
+    )
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import 'isomorphic-fetch'
+
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import Gallery from '../components/Gallery'
+
+export default class extends React.PureComponent {
+  componentDidMount () {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log('service worker registration successful')
+        })
+        .catch(err => {
+          debugger
+          console.warn('service worker registration failed', err.message)
+        })
+    }
+  }
+
+  render() {
+    return (
+      <Layout title="PWA GAllery">
+        <Header/>
+        <main>
+          <Gallery/>
+        </main>
+      </Layout>
+    )
+  }
+}
